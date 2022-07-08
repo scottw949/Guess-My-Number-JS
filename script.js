d32742d0b7067da5e4cf67f5b413599c6d9106c8
scottw949 <92278492+scottw949@users.noreply.github.com>
2022-07-07 19:36:04 -0700

    VS test commit

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/*
-//console.log('test');
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-console.log(document.querySelector('.guess').value);
-*/
-
-console.log('git test');
-
 //random number
 let randNum = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
